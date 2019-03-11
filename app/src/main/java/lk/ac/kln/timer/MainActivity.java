ea110728d2ac9c7161dab99b3f89940a540f9d46
MadhushikaTJayasinghe <jayasinghemadhushika@gmail.com>
2019-03-11 09:25:34 +0530

    test 1

--- app/src/main/java/lk/ac/kln/timer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/timer/MainActivity.java
@@ -1,5 +1,6 @@
 package lk.ac.kln.timer;
 
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,21 +8,40 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   private int counter =99;
+  private boolean running;
+  private boolean wasRunning;
+  private static final String CURRENT_COUNTER  = "COUNTER";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            counter = savedInstanceState.getInt(CURRENT_COUNTER);
+        }
         CountDown();
     }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(CURRENT_COUNTER,counter);
+    }
+
+
 
     private void CountDown(){
-        final TextView textView=findViewById(R.id.textView);
+        final Handler handler =new Handler();
+       handler.post(new Runnable() {
+           @Override
+           public void run() {
+               final TextView textView=findViewById(R.id.textView);
+               textView.setText(Integer.toString(counter));
+               //Reset the counter
+               if(counter == 0){
+                   counter = 99;
+           }
 
-         final Handler handler =new Handler();
-            handler.post(new Runnable(){
-                @Override
-                public void run(){
-                    textView.setText(Integer.toString(counter));
+
+
                     counter--;
                     handler.postDelayed(this,1000);
                 }
